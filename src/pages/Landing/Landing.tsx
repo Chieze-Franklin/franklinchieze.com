7dc5faa40e9114ecf46e6832ef7b3df8f7d0afdc
chieze-franklin <chieze.franklin@gmail.com>
2022-03-06 01:35:53 +0100

    update landing page

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -24,9 +24,8 @@ export const Landing: React.FC = () => {
                 <p className="description">
                   Full stack web developer currently working with <a href="https://www.homebound.com/" target="_blank" rel="noreferrer">Homebound</a>{" "}
                   where we are disrupting the construction industry in US and beyond.
-                  During the weekends I help build <a href="https://comicwox.com/" target="_blank" rel="noreferrer">Comicwox</a> and{" "}
-                  <a href="https://yutars.com/" target="_blank" rel="noreferrer">Yutars</a>,{" "}
-                  2 startups seeking to leave their marks on the comics and lending industries.
+                  In my spare time I build <a href="http://agnos-cloud.herokuapp.com/designs/+" target="_blank" rel="noreferrer">Agnos Cloud</a> and{" "}
+                  <a href="https://www.npmjs.com/package/graphstrap" target="_blank" rel="noreferrer">graphstrap</a>,{" "}.
                   I'm passionate about helping companies scale and achieve their goals using technology.
                 </p>
                 <Link to="/resume">
@@ -34,22 +33,16 @@ export const Landing: React.FC = () => {
                 </Link>
                 <p className="heading">News &#128071;</p>
                 <Card
-                  title="Yutars"
-                  text="I'm currently working on Yutars, a simple and modern way to know a borrower's level of credit exposure in real time."
-                  url="https://yutars.com/"
+                  title="Agnos Cloud"
+                  text="Agnos provides you with a drag-and-drop interface to help you create system designs. You can also bring your system designs to life by provisioning them on any of the popular cloud providers, right from within the app."
+                  url="http://agnos-cloud.herokuapp.com/designs/+"
                   urlText="yutars.com"
                 />
                 <Card
-                  title="graphql-bootstrap"
-                  text="I'm currently working on this CLI tool that converts TypeScript types to GraphQL schema and their TypeScript resolvers."
-                  url="https://www.npmjs.com/package/graphql-bootstrap"
+                  title="graphstrap"
+                  text="This is a CLI tool that converts TypeScript types to GraphQL schema and their TypeScript resolvers."
+                  url="https://www.npmjs.com/package/graphstrap"
                   urlText="npm package"
-                />
-                <Card
-                  title="Comicwox"
-                  text="I'm currently adding some new features to Comicwox, a platform that allows users read Nigerian-made comics."
-                  url="https://comicwox.com/"
-                  urlText="comicwox.com"
                 />
               </div>
             </div>
